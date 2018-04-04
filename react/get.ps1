d2360c682a36e21d5dfd777b172a0d280f48c261
Stephan Thierry <sth@minerva-plm.com>
2018-04-04 09:45:23 +0200

    first commit

---
+++ react/get.ps1
@@ -0,0 +1,8 @@
+
+./wget http://coenraets.org/present/react/assets/css/ionic.css  /assets/css/
+./wget http://coenraets.org/present/react/assets/css/keypoint.css /assets/css/
+./wget http://coenraets.org/present/react/assets/css/prettify.css /assets/css/
+./wget http://coenraets.org/present/react/assets/css/slides.css /assets/css/
+
+./wget http://coenraets.org/present/react/img/react.png /img
+./wget http://coenraets.org/present/react/img/0days.jpg /img
